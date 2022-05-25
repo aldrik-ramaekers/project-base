935c3a5ebebef41cc0de8e934c84833b8ff2b515
Aldrik Ramaekers <aldrik@amftech.nl>
2022-05-25 17:29:26 +0200

    remove old code

--- src/qui/label.c
+++ src/qui/label.c
@@ -1,56 +1,3 @@
-#if 0
-void _qui_update_label(qui_widget* el) {
-	qui_label* data = ((qui_label*)el->data);
-	s32 tw = renderer->calculate_text_width(global_ui_context.font_small, 
-			(data->text)) + (LABEL_PADDING_W*2);
-	
-	el->width = tw;
-	if (el->width > el->parent->width) el->width = el->parent->width;
-}
-
-
-void _qui_render_label(qui_widget* el) {
-	qui_label* data = ((qui_label*)el->data);
-
-	if (data->text) {
-		char* text = data->text;
-		s32 tw = renderer->calculate_text_width(global_ui_context.font_small, text);
-
-		if (el->width == tw) {
-		renderer->render_text(global_ui_context.font_small, 
-			el->x,
-			el->y + (el->height/2)-(global_ui_context.font_small->px_h/2), // Label is single line so center it on y-axis.
-			text, 
-			active_ui_style.widget_text);
-			
-		}
-		else {
-			// TODO: we should really have a function to calculate the height of cutoff text before rendering it..
-			el->height = renderer->render_text_cutoff(global_ui_context.font_small, 
-				el->x,
-				el->y,
-				text, 
-				active_ui_style.widget_text, el->width);
-		}
-	}
-}
-
-qui_widget* qui_create_label(qui_widget* qui, char* text, bool wrap)
-{
-	log_assert(qui->type == WIDGET_VERTICAL_LAYOUT || qui->type == WIDGET_HORIZONTAL_LAYOUT, "Label can only be added to vertical or horizontal layout");
-	qui_widget* wg = _qui_create_empty_widget(qui);
-	qui_label* data = mem_alloc(sizeof(qui_label));
-	data->text = text;
-	wg->data = (u8*)data;
-	wg->type = WIDGET_LABEL;
-	wg->height = global_ui_context.font_small->px_h;
-	wg->margin_x = INTERACTIVE_ELEMENT_MARGIN_W;
-	wg->margin_y = INTERACTIVE_ELEMENT_MARGIN_H;
-	return wg;
-}
-#endif
-
-#if 1
 
 void _qui_update_label(qui_widget* el) {
 	qui_label* data = ((qui_label*)el->data);
@@ -104,6 +51,8 @@ qui_widget* qui_create_label(qui_widget* qui, char* text, bool allow_wrap)
 	log_assert(qui->type == WIDGET_VERTICAL_LAYOUT || qui->type == WIDGET_HORIZONTAL_LAYOUT, "Label can only be added to vertical or horizontal layout");
 
 	qui_widget* og = qui;
+
+	// When we have a multi-line label on a horizontal layout we create a flex container for it.
 	if (allow_wrap && qui->type == WIDGET_HORIZONTAL_LAYOUT) {
 		qui_widget* text_container = qui_create_flex_container(qui, 1);
 		qui = text_container;
@@ -121,4 +70,3 @@ qui_widget* qui_create_label(qui_widget* qui, char* text, bool allow_wrap)
 	wg->margin_y = INTERACTIVE_ELEMENT_MARGIN_H;
 	return wg;
 }
-#endif
